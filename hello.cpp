9d6d98359126496f9f8c72b61496267fb1e9bc51
aruneshroy91 <arunesroy@gmail.com>
2023-12-24 10:51:56 +0530

    second commit cpp

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+/* First c++ program for practice, comments are written for explanation */
+#include <iostream> /*preprocessor commands*/
+
+ /*Code that executes when program starts up
+cout<< is used for output to screen
+:: is the "scope resolution operator"
+ */
 
 int main()
 {
-    std::cout <<"Hello World" << std::endl;
+    std::cout <<"Hello World! \n" << std::endl;
+
+    return 0; 
 }
