612e41ef90a7faaf2081c79f9c9c8f12bac9bd6e
aruneshroy91 <arunesroy@gmail.com>
2023-12-26 00:38:07 +0530

    variables 2

--- variables/variables1.cpp
+++ variables/variables1.cpp
@@ -2,8 +2,10 @@
 using namespace std;
 
 int main(){
-    int x;
-    x = 4 + 2;
+    int x; /*Declaration of variable x*/
+    x = 4 + 2; /*Initialization of x, = being the assignment operator*/
+    /* A cleaner version of 
+    declaration and initialization -->  int x = 4 + 2;*/
     /*Printing a sequence of values by chaining << symbol*/
     std::cout << x/3 <<'\n'<< x*2 << std::endl; 
 
