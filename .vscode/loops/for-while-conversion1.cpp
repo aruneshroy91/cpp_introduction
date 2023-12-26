1b9cd1f256de3112547f2a25e242620a23c62989
aruneshroy91 <arunesroy@gmail.com>
2023-12-26 09:08:04 +0530

    nested control structures

---
+++ .vscode/loops/for-while-conversion1.cpp
@@ -0,0 +1,28 @@
+/*#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 0;
+
+    while(x<10)
+        x = x+1;
+    cout << "x is " << x << "\n";
+
+    return 0;
+}*/
+
+/*While loop for the above for loop*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x = 0;
+    while(x<10){
+        cout << x << "\n";
+        x = x + 1;
+    }
+
+    return 0;
+}
+
