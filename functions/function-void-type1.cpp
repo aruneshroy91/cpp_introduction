5287494cf983976f8320a842e8d8fa7ada225a4c
aruneshroy91 <arunesroy@gmail.com>
2023-12-28 08:57:07 +0530

    using function of void type

---
+++ functions/function-void-type1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+/*Void return type: returns nothing*/
+void printNumber(int num){
+    cout << "number is " << num << endl; 
+}
+
+int main(){
+    printNumber(4); /* number is 4*/
+    return 0;
+}
